3c07d8e1794817587afbf3e56f7f752f73930b39
Harshavardhan <buildingnationwithtrust@gmail.com>
2024-01-31 16:44:52 +0530

    header navigation started

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  title = 'Naviagation'
+  menuType:string = ''
+
+
+  constructor(private route:Router) {}
+
+  ngOnInit(): void {
+    this.route.events.subscribe((route:any) => {
+      if(route.url) {
+        console.warn('Route-',route.url);
+        
+        if(localStorage.getItem('seller') && route.url.includes('seller')) {
+          console.warn('Inside Seller Are');
+          this.menuType = 'seller';
+
+        }  else {
+          console.warn('Inside Default Area ');
+          this.menuType = 'default';
+      } 
+      }
+    })
+  }
+
+
+
 
 }
